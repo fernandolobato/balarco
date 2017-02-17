061d635c510c191800522e88e00fdc0e900c3aa6
Julian NG <juliannieb@gmail.com>
2017-02-16 20:32:10 -0600

    DjangoRestFramework config

--- balarco/urls.py
+++ balarco/urls.py
@@ -19,4 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url('', include('base.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^clients/', include('clients.urls'))
 ]
