c215b2f53a6067892dffc8a00df388298babda6f
Marco Lopez <marcolm485@gmail.com>
2017-02-20 09:54:13 -0600

    Fix styling

--- clients/models.py
+++ clients/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-class Client(models.Model):
-	name = models.CharField(max_length=100)
-	address = models.CharField(max_length=100)
 
-	def __str__(self):
-		return self.name
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
