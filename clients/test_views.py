1571897e57ee255587dd693f53e9705fa72b0b83
Julian NG <juliannieb@gmail.com>
2017-02-21 22:04:31 -0600

    Add tests for clients API

--- clients/test_views.py
+++ clients/test_views.py
@@ -4,7 +4,7 @@ from rest_framework.test import APITestCase, APIRequestFactory, force_authentica
 from rest_framework import status
 
 from .models import Client, Contact
-from .views import ContactViewSet
+from .views import ContactViewSet, ClientViewSet
 
 
 class ContactTest(APITestCase):
@@ -87,8 +87,8 @@ class ContactTest(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(self.number_of_contacts, len(response.data))
-        for client in response.data:
-            self.assertEqual(client['name'], Contact.objects.get(id=client['id']).name)
+        for contact in response.data:
+            self.assertEqual(contact['name'], Contact.objects.get(id=contact['id']).name)
 
     def test_empty_contact_creation(self):
         """
@@ -105,7 +105,7 @@ class ContactTest(APITestCase):
 
     def test_modify_contact(self):
         """
-            Test that a client object can be modified.
+            Test that a contact object can be modified.
         """
         data = {
             'name': 'Julian',
@@ -128,4 +128,103 @@ class ContactTest(APITestCase):
 
         instance_update = Contact.objects.get(id=self.contact_instance_julian.id)
         self.assertEqual(instance_update.phone, '66666666')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class ClientTest(APITestCase):
+    """
+        Tests to verify the basic usage of the REST API to create, modify and list clients.
+        @TODO: Token authentication is not yet enabled. Once it's enabled, the
+        force_authenticate methods should recieve the user and token as params.
+    """
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='test_user',
+                                             password='test_password')
+
+        self.client_instance_starbucks = Client.objects.create(
+            name='Test Starbucks',
+            address='Felipe Ángeles 225',
+            is_active=True)
+
+        self.client_instance_oxxo = Client.objects.create(
+            name='OXXO',
+            address='Epigmenio González 128',
+            is_active=True)
+
+        self.number_of_clients = 2
+        self.url_list = 'clients:Clients-list'
+        self.url_detail = 'clients:Clients-detail'
+        self.factory = APIRequestFactory()
+
+    def test_client_creation(self):
+        """
+            Test that a client instance can be generated through the REST API endpoint.
+        """
+
+        data = {
+            'name': 'COMEX',
+            'address': 'Calle 5 de Mayo 350',
+            'is_active': True}
+        request = self.factory.post(reverse(self.url_list), data=data)
+        force_authenticate(request, user=self.user)
+        view = ClientViewSet.as_view({
+                                      'post': 'create',
+                                      })
+        response = view(request)
+        client_instance = Client.objects.get(id=response.data['id'])
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual('COMEX', client_instance.name)
+        self.assertEqual(self.number_of_clients + 1, Client.objects.all().count())
+
+    def test_multiple_client_listing(self):
+        """
+            Tests that all client objects can be retrieved through the REST API endpoint.
+        """
+        request = self.factory.get(reverse(self.url_list))
+        force_authenticate(request, user=self.user)
+        view = ClientViewSet.as_view({
+                                      'get': 'list',
+                                      })
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.number_of_clients, len(response.data))
+        for client in response.data:
+            self.assertEqual(client['name'], Client.objects.get(id=client['id']).name)
+
+    def test_empty_client_creation(self):
+        """
+            Tests that a client object can't be created without the required information.
+        """
+        data = {}
+        request = self.factory.post(reverse(self.url_list), data=data)
+        force_authenticate(request, user=self.user)
+        view = ClientViewSet.as_view({
+                                      'post': 'create',
+                                      })
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_modify_contact(self):
+        """
+            Test that a client object can be modified.
+        """
+        data = {
+            'name': 'OXXO',
+            'address': 'Bernardo Quintana 221',
+            'is_active': True
+            }
+        request = self.factory.put(reverse(self.url_detail,
+                                           kwargs={'pk': self.client_instance_oxxo.id}),
+                                   data=data)
+        force_authenticate(request, user=self.user)
+        view = ClientViewSet.as_view({
+                                      'put': 'update',
+                                      })
+        response = view(request, pk=self.client_instance_oxxo.id)
+
+        instance_update = Client.objects.get(id=self.client_instance_oxxo.id)
+        self.assertEqual(instance_update.address, 'Bernardo Quintana 221')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
