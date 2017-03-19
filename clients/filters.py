f01fccce7fa5e5018f76b359a44b2ca91ef66e47
Julian NG <juliannieb@gmail.com>
2017-03-18 21:14:06 -0600

    Add filters to client's app REST services

---
+++ clients/filters.py
@@ -0,0 +1,15 @@
+import django_filters
+from . import models as client_models
+
+
+class ClientFilter(django_filters.rest_framework.FilterSet):
+    class Meta:
+        model = client_models.Client
+        fields = ['id', 'name', 'address']
+
+
+class ContactFilter(django_filters.rest_framework.FilterSet):
+    class Meta:
+        model = client_models.Contact
+        fields = ['id', 'client', 'name', 'last_name', 'charge', 'landline', 'extension',
+                  'mobile_phone_1', 'mobile_phone_2', 'email', 'alternate_email']
--- clients/views.py
+++ clients/views.py
@@ -1,6 +1,9 @@
+import django_filters.rest_framework
+
 from .models import Client, Contact
 from .serializers import ClientSerializer, ContactSerializer
 from balarco import utils
+from . import filters as client_filters
 
 
 class ContactViewSet(utils.GenericViewSet):
@@ -9,6 +12,8 @@ class ContactViewSet(utils.GenericViewSet):
     obj_class = Contact
     queryset = Contact.objects.filter(is_active=True)
     serializer_class = ContactSerializer
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+    filter_class = client_filters.ContactFilter
 
 
 class ClientViewSet(utils.GenericViewSet):
@@ -17,3 +22,5 @@ class ClientViewSet(utils.GenericViewSet):
     obj_class = Client
     queryset = Client.objects.filter(is_active=True)
     serializer_class = ClientSerializer
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+    filter_class = client_filters.ClientFilter
