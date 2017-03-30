b43b34418d244acee363485d42a23694ed9d654f
Julian NG <juliannieb@gmail.com>
2017-03-30 16:49:37 -0600

    Add fields to work serializer for GET methods

--- works/serializers.py
+++ works/serializers.py
@@ -50,36 +50,6 @@ class StatusSerializer(serializers.ModelSerializer):
         fields = ('id', 'status_id', 'name',)
 
 
-class WorkSerializer(serializers.ModelSerializer):
-
-    creation_date = serializers.DateField(read_only=True)
-    executive_complete = user_serializers.UserSerializer(source='executive', read_only=True)
-    contact_complete = client_serializers.ContactSerializer(source='contact', read_only=True)
-    current_status_complete = StatusSerializer(source='current_status', read_only=True)
-    work_type_complete = WorkTypeSerializer(source='work_type', read_only=True)
-    iguala_complete = IgualaSerializer(source='iguala', read_only=True)
-
-    class Meta:
-        model = models.Work
-        fields = ('id',
-                  'executive',
-                  'executive_complete',
-                  'contact',
-                  'contact_complete',
-                  'current_status',
-                  'current_status_complete',
-                  'work_type',
-                  'work_type_complete',
-                  'iguala',
-                  'iguala_complete',
-                  'creation_date',
-                  'name',
-                  'expected_delivery_date',
-                  'brief',
-                  'final_link',
-                  )
-
-
 class ArtWorkSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -111,3 +81,42 @@ class StatusChangeSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.StatusChange
         fields = ('id', 'work', 'status', 'user', 'date',)
+
+
+class WorkSerializer(serializers.ModelSerializer):
+
+    creation_date = serializers.DateField(read_only=True)
+    executive_complete = user_serializers.UserSerializer(source='executive', read_only=True)
+    contact_complete = client_serializers.ContactSerializer(source='contact', read_only=True)
+    current_status_complete = StatusSerializer(source='current_status', read_only=True)
+    work_type_complete = WorkTypeSerializer(source='work_type', read_only=True)
+    iguala_complete = IgualaSerializer(source='iguala', read_only=True)
+
+    art_works = ArtWorkSerializer(many=True, read_only=True)
+    files = FileSerializer(many=True, read_only=True)
+    work_designers = WorkDesignerSerializer(many=True, read_only=True)
+    status_changes = StatusChangeSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = models.Work
+        fields = ('id',
+                  'executive',
+                  'executive_complete',
+                  'contact',
+                  'contact_complete',
+                  'current_status',
+                  'current_status_complete',
+                  'work_type',
+                  'work_type_complete',
+                  'iguala',
+                  'iguala_complete',
+                  'creation_date',
+                  'name',
+                  'expected_delivery_date',
+                  'brief',
+                  'final_link',
+                  'art_works',
+                  'files',
+                  'work_designers',
+                  'status_changes'
+                  )
