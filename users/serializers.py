e01ebdd091fbc7c32b0c1e708c010ae74f9bd2a7
Marco Lopez <marcolm485@gmail.com>
2017-03-31 11:58:23 -0600

    Add complete group serializer

--- users/serializers.py
+++ users/serializers.py
@@ -3,16 +3,26 @@ from django.contrib.auth.models import User, Group
 
 
 class GroupSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Group
         fields = ('id', 'name',)
 
 
 class UserSerializer(serializers.ModelSerializer):
+
+    groups_complete = GroupSerializer(source='groups', many=True, read_only=True)
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'password', 'first_name',
-                  'last_name', 'groups', )
+        fields = ('id',
+                  'username',
+                  'password',
+                  'first_name',
+                  'last_name',
+                  'groups',
+                  'groups_complete',
+                  )
         extra_kwargs = {
                         'username': {'required': True},
                         'password': {'required': False}
